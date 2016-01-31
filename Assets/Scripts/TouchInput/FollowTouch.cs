87883963fb798c474afcf6ad80190ec69876a388
Rylie Lallo <Rylie Lallo>
2016-01-30 19:16:02 -0800

    Working on gesture recognition

--- Assets/Scripts/TouchInput/FollowTouch.cs
+++ Assets/Scripts/TouchInput/FollowTouch.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class FollowTouch : MonoBehaviour {
 
-    private bool isTouching = false;
-
-    public SpriteRenderer city;
-    public SpriteRenderer mountain;
-
 	// Use this for initialization
 	void Start () {
 	
@@ -16,17 +11,8 @@ public class FollowTouch : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            city.enabled = true;
-            mountain.enabled = false;
-        }
-        else if (Input.GetTouch(0).phase == TouchPhase.Ended)
-        {
-            city.enabled = false;
-            mountain.enabled = true;
-        }
+        Touch myTouch = Input.GetTouch(0);
 
-
+        //Detect swipe
     }
 }
