5547afb660944ccae01657ffd5069edb8e14d018
Rylie Lallo <Rylie Lallo>
2016-01-30 18:04:39 -0800

    confirmed touch inputs work

---
+++ Assets/Scripts/TouchInput/FollowTouch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTouch : MonoBehaviour {
+
+    private bool isTouching = false;
+
+    public SpriteRenderer city;
+    public SpriteRenderer mountain;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            city.enabled = true;
+            mountain.enabled = false;
+        }
+        else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            city.enabled = false;
+            mountain.enabled = true;
+        }
+
+
+    }
+}
