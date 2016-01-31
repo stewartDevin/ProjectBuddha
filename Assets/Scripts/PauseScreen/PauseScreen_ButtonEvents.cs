df0352364249d6ec153480bf525ea355a76549e8
Devin Stewart <devinstewart@stu.aii.edu>
2016-01-30 19:14:22 -0800

    added title screen and pause screen

---
+++ Assets/Scripts/PauseScreen/PauseScreen_ButtonEvents.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseScreen_ButtonEvents : MonoBehaviour {
+
+    public GameObject pauseScreenPanel;
+
+    public void OnPauseButtonPressed()
+    {
+        pauseScreenPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void OnResumeButtonPressed()
+    {
+        pauseScreenPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OnExitButtonPressed()
+    {
+        Application.LoadLevel("TitleScreen");
+        Time.timeScale = 1f;
+    }
+
+    
+}
